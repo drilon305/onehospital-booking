fdfc0d0031b312522e2584b463655616e9bbb291
Drilon <navycrack1@gmail.com>
2023-10-20 23:54:28 +0200

    Initial commit

---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from '../pages/Home'
+import Services from '../pages/Services'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import Contact from '../pages/Contact'
+import Doctors from '../pages/Doctors/Doctors'
+import DoctorDetails from '../pages/Doctors/DoctorDetail'
+
+
+const Routers = () => {
+  return (
+    <div>Routers</div>
+  )
+}
+
+export default Routers
