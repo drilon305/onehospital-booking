dde8ef9a972c4745f4e27e996c204146d0955df7
Drilon <navycrack1@gmail.com>
2023-11-05 16:24:02 +0100

    added experience with a beauitful design

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { formateDate } from '../../utils/formateDate'
 
 
@@ -31,9 +31,9 @@ const DoctorAbout = () => {
           <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
             <div>
               <span className="text-[15px] text-irisBlueColor leading-6 font-semibold">
-              {formateDate("09-13-2014")}  -  {formateDate("06-13-2016")}
+                {formateDate("09-13-2014")} - {formateDate("06-13-2016")}
               </span>
-              <p className="text-[16px] leading-6 font-medium text-textColor">
+              <p className="text-[15px] leading-6 font-medium text-textColor">
                 PHD in Surgeon
               </p>
             </div>
@@ -44,12 +44,44 @@ const DoctorAbout = () => {
           <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
             <div>
               <span className="text-[15px] text-irisBlueColor leading-6 font-semibold">
-                {formateDate("07-04-2010")}  -  {formateDate("08-13-2014")}
+                {formateDate("07-04-2010")} - {formateDate("08-13-2014")}
               </span>
-              <p className="text-[16px] leading-6 font-medium text-textColor">
+              <p className="text-[15px] leading-6 font-medium text-textColor">
                 PHD in Surgeon
               </p>
             </div>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              Acibadem Sistina Hospital
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Experience
+        </h3>
+
+        <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formateDate("07-04-2010")} - {formateDate("08-13-2014")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              Acibadem Sistina Hospital
+            </p>
+          </li>
+
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+            {formateDate("09-13-2014")} - {formateDate("06-13-2016")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
             <p className="text-[14px] leading-5 font-medium text-textColor">
               Acibadem Sistina Hospital
             </p>
