86cfff8744e715bb341fee5edb15b4d4d821ec49
Drilon <navycrack1@gmail.com>
2023-11-13 00:16:37 +0100

    adding backend for onehospital

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+    origin: true
+}
+
+app.get('/', (req, res) => {
+    res.send('Api is working')
+})
+
+// database connection
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Mongodb database is connected");
+  } catch (err) {
+    console.log("Mongodb database is connection failed");
+  }
+};
+
+
+
+//middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+
+app.listen(port, () => {
+    connectDB();
+    console.log('Server is running on port ' + port)
+})
