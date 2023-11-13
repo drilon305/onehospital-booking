0e7d839456bfdd06d851ae12c733ac782943c1a6
Drilon <navycrack1@gmail.com>
2023-11-14 00:02:09 +0100

    register backend

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import authRoute from './Routes/auth.js'
 
 
 dotenv.config();
@@ -23,8 +24,6 @@ mongoose.set("strictQuery", false);
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
 
     console.log("Mongodb database is connected");
@@ -39,6 +38,7 @@ const connectDB = async () => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
+app.use('/api/v1/auth', authRoute)
 
 
 app.listen(port, () => {
