aa8a6efa3f7c540e919904dd7ee37c49911ca1f2
Drilon <navycrack1@gmail.com>
2023-11-18 23:13:00 +0100

    create review

--- backend/Controllers/doctorController.js
+++ backend/Controllers/doctorController.js
@@ -1,44 +1,52 @@
-import Doctor from "../models/DoctorSchema.js"
+import Doctor from "../models/DoctorSchema.js";
 
-export const updateDoctor = async(req, res) => {
-    const id = req.params.body;
+export const updateDoctor = async (req, res) => {
+  const id = req.params.id;
 
+  try {
+    const updatedDoctor = await Doctor.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    );
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "Successfully updated",
+        data: updatedDoctor,
+      });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Failed to update" });
+  }
+};
 
-    try {
-        const updatedDoctor = await Doctor.findByIdAndUpdate(id, {$set: req.body}, {new:true});
-        res.status(200).json({ success: true, message: 'Successfully updated', data: updatedDoctor})
+export const deleteDoctor = async (req, res) => {
+  const id = req.params.id;
 
-    } catch (error) {
-        res.status(500).json({ success: false, message: 'Failed to update'})
-    }
-}
+  try {
+    await Doctor.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Successfully deleted" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Failed to delete" });
+  }
+};
 
-export const deletDoctor = async(req, res) => {
-    const id = req.params.body;
+export const getSingleDoctor = async (req, res) => {
+  const id = req.params.id;
 
+  try {
+    const doctor = await Doctor.findById(id)
+      .populate("reviews")
+      .select("-password");
 
-    try {
-       await Doctor.findByIdAndDelete(id);
-        res.status(200).json({ success: true, message: 'Successfully deleted'})
-
-    } catch (error) {
-        res.status(500).json({ success: false, message: 'Failed to delete'})
-    }
-}
-
-export const getSingleDoctor = async(req, res) => {
-    const id = req.params.body;
-
-
-    try {
-        const doctor = await Doctor.findById(id).select('-password');
-
-        res.status(200).json({ success: true, message: 'Doctor found', data: doctor})
-
-    } catch (error) {
-        res.status(404).json({ success: false, message: 'No Doctor found'})
-    }
-}
+    res
+      .status(200)
+      .json({ success: true, message: "Doctor found", data: doctor });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "No Doctor found" });
+  }
+};
 
 export const getAllDoctor = async (req, res) => {
   try {
