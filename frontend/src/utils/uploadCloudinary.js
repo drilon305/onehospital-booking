35627f36f079458cd24cd7976a9f3e42e53ad87b
Drilon <navycrack1@gmail.com>
2023-11-22 21:10:55 +0100

    fixing small issue

--- frontend/src/utils/uploadCloudinary.js
+++ frontend/src/utils/uploadCloudinary.js
@@ -9,7 +9,7 @@ const uploadImageToCloudinary = async (file) => {
   uploadData.append("cloud_name", cloud_name);
 
   const res = await fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, {
-    method: 'POST',
+    method: 'post',
     body: uploadData
   })
 
