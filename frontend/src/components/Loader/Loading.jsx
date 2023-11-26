3a93d659ef2e6e63263823801fda743b6b204458
Drilon <navycrack1@gmail.com>
2023-11-27 00:32:03 +0100

    designed profile and loading

---
+++ frontend/src/components/Loader/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HashLoader from 'react-spinners/HashLoader'
+
+const Loading = () => {
+  return (
+    <div className='w-full h-full flex items-center justify-center'><HashLoader color='#0067FF' /></div>
+  )
+}
+
+export default Loading
